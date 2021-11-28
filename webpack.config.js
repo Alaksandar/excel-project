6944201186816199fc244bbb94c640f879ae75d8
alaksandar_km <ebenzomarket@gmail.com>
2021-11-28 20:15:46 +0100

    add ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 const path = require('path')
+const {plugins} = require("@babel/preset-env/lib/plugins-compat-data");
 
 module.exports = (env, argv) => {
 
     const isProd = argv.mode === 'production'
 
     const filename = (ext) => isProd ? `[name].[contenthash:8].bundle.${ext}` : `[name].bundle.${ext}`
+
+    const  plugins = () => {
+        const base = [
+            new HtmlWebpackPlugin({
+                    template: './index.html'
+                }
+            ),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                        to: path.resolve(__dirname, 'dist')
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: filename('css')
+            }),
+        ]
+        console.log(!isProd)
+        !isProd ? base.push(new ESLintPlugin()) : false
+
+        return base
+    }
 
     return {
         target: 'web',
@@ -32,29 +58,13 @@ module.exports = (env, argv) => {
             }
         },
         devServer: {
-            port: 3000,
+            port: 8800,
             open: true,
             hot: true,
             watchFiles: './',
         },
-        devtool: isProd ? false : 'source-map',
-        plugins: [
-            new HtmlWebpackPlugin({
-                    template: './index.html'
-                }
-            ),
-            new CopyPlugin({
-                patterns: [
-                    {
-                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
-                        to: path.resolve(__dirname, 'dist')
-                    },
-                ],
-            }),
-            new MiniCssExtractPlugin({
-                filename: filename('css')
-            }),
-        ],
+        devtool: !isProd ? 'source-map' : false,
+        plugins: plugins(),
         module: {
             rules: [
                 {
